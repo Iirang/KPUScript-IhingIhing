db345187d7b2d6812f8f6d75252063138b2e6148
DESKTOP-KJDL4HB\USER <meekk1009@gmail.com>
2021-06-07 23:58:31 +0900

    Notebook으로 리스트 선택 결과를 출력할 페이지 분리, 라벨 추가, 리스트에서 무언가를 선택했을 때 호출되는 함수 bind, font 종류 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.ttk
 from tkinter import font
 from Horse import *
 from raceResult import *
@@ -61,6 +62,14 @@ class MainGUI():
 
     def SendMail(self):
         GetMail()
+    
+    def CurSelect(self, evt):   # evt를 사용하지 않더라도, tkinter에서 이벤트를 설명하는 객체를 호출하기 때문에 evt를 매개변수로 추가해야 한다.
+        index = self.HorseListbox.index(self.HorseListbox.curselection())
+        print(index)
+
+        InfoList = self.HorseInfoList[index].GetInfoList()
+
+        #self.raceInfoLabel['text'] = 
 
     def __init__(self):
         self.window = Tk()
@@ -70,7 +79,8 @@ class MainGUI():
         self.window['bg'] = 'white'
 
         self.LogoImage = PhotoImage(file='Resource/Logo_Up_b.gif')
-        self.font = font.Font(size=15, weight='bold', family='맑은 고딕')
+        self.font1 = font.Font(size=15, weight='bold', family='맑은 고딕')
+        self.font2 = font.Font(size=10, weight='bold', family='맑은 고딕')
 
         LogoFrame = Frame(self.window, width=800, height=110, bg='white')
         LogoFrame.pack()
@@ -82,11 +92,11 @@ class MainGUI():
                                                         # Entry 개수: 지역(1)+날짜(3)+경주 번호(1) = 5
         SearchFrame.pack()
 
-        self.SearchLabel.append(Label(SearchFrame, text='지역', font=self.font, bg='white'))
-        self.SearchLabel.append(Label(SearchFrame, text='년', font=self.font, bg='white'))
-        self.SearchLabel.append(Label(SearchFrame, text='월', font=self.font, bg='white'))
-        self.SearchLabel.append(Label(SearchFrame, text='일', font=self.font, bg='white'))
-        self.SearchLabel.append(Label(SearchFrame, text='경주 번호', font=self.font, bg='white'))
+        self.SearchLabel.append(Label(SearchFrame, text='지역', font=self.font1, bg='white'))
+        self.SearchLabel.append(Label(SearchFrame, text='년', font=self.font1, bg='white'))
+        self.SearchLabel.append(Label(SearchFrame, text='월', font=self.font1, bg='white'))
+        self.SearchLabel.append(Label(SearchFrame, text='일', font=self.font1, bg='white'))
+        self.SearchLabel.append(Label(SearchFrame, text='경주 번호', font=self.font1, bg='white'))
 
         self.meet = StringVar()     # 지역
         self.year = StringVar()     # 년도
@@ -94,29 +104,29 @@ class MainGUI():
         self.date = StringVar()     # 일
         self.rcNum = StringVar()    # 경주 번호
 
-        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.meet, width=5, font=self.font, justify=RIGHT))
-        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.year, width=5, font=self.font, justify=RIGHT))
-        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.month, width=5, font=self.font, justify=RIGHT))
-        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.date, width=5, font=self.font, justify=RIGHT))
-        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.rcNum, width=5, font=self.font, justify=RIGHT))
+        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.meet, width=5, font=self.font1, justify=RIGHT))
+        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.year, width=5, font=self.font1, justify=RIGHT))
+        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.month, width=5, font=self.font1, justify=RIGHT))
+        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.date, width=5, font=self.font1, justify=RIGHT))
+        self.SearchEntry.append(Entry(SearchFrame, textvariable=self.rcNum, width=5, font=self.font1, justify=RIGHT))
 
         for i in range(5):
             self.SearchEntry[i].pack(padx=(10, 0), side=LEFT)
             self.SearchLabel[i].pack(padx=(10, 0), side=LEFT)
 
-        Button(SearchFrame, text='검색', command=self.Search, font=self.font).pack(padx=(10, 0), side=LEFT)
-        Button(SearchFrame, text='지도', command=self.ShowMap, font=self.font).pack(padx=(10, 0), side=LEFT)  # TODO: text 없애고, 지도 이미지 추가
+        Button(SearchFrame, text='검색', command=self.Search, font=self.font1).pack(padx=(10, 0), side=LEFT)
+        Button(SearchFrame, text='지도', command=self.ShowMap, font=self.font1).pack(padx=(10, 0), side=LEFT)  # TODO: text 없애고, 지도 이미지 추가
 
         OutputFrame = Frame(self.window, height=50, bg='white')    # 보여줄 것: 지역, 날짜, 날씨, 주로 상태(건조주로(1% ~ 5%) , 양호주로(6% ~ 9%), 다습주로(10% ~ 14%), 포화주로(15% ~ 19%), 불량주로(20% 이상))
         OutputFrame.pack()
 
-        self.OutputLabel = Label(OutputFrame, text='\t\t검색 값을 입력하고 버튼을 눌러주세요.\t\t', font=self.font, bg='white')
+        self.OutputLabel = Label(OutputFrame, text='\t\t검색 값을 입력하고 버튼을 눌러주세요.\t\t', font=self.font1, bg='white')
         self.OutputLabel.pack(side=LEFT)
 
         HorseFrame = Frame(self.window, bg='white')
         HorseFrame.pack()
         HorseScrollbar = Scrollbar(HorseFrame, orient=VERTICAL)
-        self.HorseListbox = Listbox(HorseFrame, font= self.font, height=8)  # 선택하면 우측 Listbox에 정보 출력
+        self.HorseListbox = Listbox(HorseFrame, font= self.font1, height=8, selectmode=SINGLE)  # 선택하면 우측 Listbox에 정보 출력
         self.HorseListbox.pack(side=LEFT)
         HorseScrollbar.pack(side=LEFT, fill=Y)
 
@@ -126,21 +136,29 @@ class MainGUI():
         for i in range(10):
             self.HorseListbox.insert(i, str(i+1)+'번 우승마')
         
-        HorseInfoScrollbar = Scrollbar(HorseFrame, orient=VERTICAL)
-        self.HorseInfoListbox = Listbox(HorseFrame, font=self.font, height=8)
-        self.HorseInfoListbox.pack(padx=(10, 0), side=LEFT)
-        HorseInfoScrollbar.pack(side=LEFT, fill=Y)
+        self.HorseListbox.bind('<<ListboxSelect>>', self.CurSelect) # 리스트에서 말을 선택하였을 때, self.CurSelect가 호출된다.
 
-        self.HorseInfoListbox.config(yscrollcommand=HorseScrollbar.set)
-        HorseInfoScrollbar.config(command=self.HorseInfoListbox.yview)
+        self.HorseInfoNotebook = tkinter.ttk.Notebook(HorseFrame, width=220, height=220)
+        self.HorseInfoNotebook.pack(padx=(10, 0), side=LEFT)
+
+        self.raceInfoFrame = Frame(self.window, width=220, height=220, bg='white')
+        self.raceHorseInfoFrame = Frame(self.window, width=220, height=220, bg='white')
+        self.HorseInfoNotebook.add(self.raceInfoFrame, text='경주기록 정보')
+        self.HorseInfoNotebook.add(self.raceHorseInfoFrame, text='경주마 상세정보')
+
+        self.raceInfoLabel = Label(self.raceInfoFrame, bg='white', justify=LEFT, text='경기를 입력하고,\n좌측 리스트에서 말을 선택해주세요.', font=self.font2)
+        self.raceInfoLabel.pack()
+
+        self.raceHorseInfoLabel = Label(self.raceHorseInfoFrame, bg='white', justify=LEFT, text='경기를 입력하고,\n좌측 리스트에서 말을 선택해주세요.', font=self.font2)
+        self.raceHorseInfoLabel.pack()
 
         self.canvas = Canvas(HorseFrame, width=220, height=235, bg='white') # 그래프를 출력할 canvas
         self.canvas.pack(padx=(10, 0), side=LEFT)                           # TODO: 승률 비교 그래프 그리는 함수 추가
 
         ButtonFrame = Frame(self.window, bg='white')
         ButtonFrame.pack()
-        Button(ButtonFrame, text='텔레그램', font=self.font).pack(padx=(590, 0), side=LEFT) # TODO: 텔레그램 봇 연동
-        Button(ButtonFrame, text='Gmail', command=self.SendMail, font=self.font).pack(padx=(10, 0), side=LEFT) # TODO: Gmail 연동
+        Button(ButtonFrame, text='텔레그램', font=self.font1).pack(padx=(590, 0), side=LEFT) # TODO: 텔레그램 봇 연동
+        Button(ButtonFrame, text='Gmail', command=self.SendMail, font=self.font1).pack(padx=(10, 0), side=LEFT) # TODO: Gmail 연동
 
         # 아래 칸 로고
         self.DownLogoImage = PhotoImage(file='Resource/Logo_Down_a.gif')
