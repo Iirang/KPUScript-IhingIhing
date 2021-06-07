a69b768aa19871dd4d8c6322abf91acd788e4fec
DESKTOP-KJDL4HB\USER <meekk1009@gmail.com>
2021-06-07 23:56:33 +0900

    배열을 추가해서 함수 내용 간략하게 수정

--- Horse.py
+++ Horse.py
@@ -38,107 +38,13 @@ CHAKSUNT = 22
 
 class Horse:
     def __init__(self):
-        pass
+        self.InfoList = [''] * 23
 
     def SetInfo(self, data, index):
-        if index == 0:
-            self.hrName = data
-        elif index == 1:
-            self.hrNo = data
-        elif index == 2:
-            self.name = data
-        elif index == 3:
-            self.age = data
-        elif index == 4:
-            self.sex = data
-        elif index == 5:
-            self.ord = data
-        elif index == 6:
-            self.owName = data
-        elif index == 7:
-            self.owNo = data
-        elif index == 8:
-            self.birthday = data
-        elif index == 9:
-            self.rank = data
-        elif index == 10:
-            self.faHrName = data
-        elif index == 11:
-            self.faHrNo = data
-        elif index == 12:
-            self.moHrName = data
-        elif index == 13:
-            self.moHrNo = data
-        elif index == 14:
-            self.rcCntT = data
-        elif index == 15:
-            self.ord1CntT = data
-        elif index == 16:
-            self.ord2CntT = data
-        elif index == 17:
-            self.ord3CntT = data
-        elif index == 18:
-            self.rcCntY = data
-        elif index == 19:
-            self.ord1CntY = data
-        elif index == 20:
-            self.ord2CntY = data
-        elif index == 21:
-            self.ord3CntY = data
-        elif index == 22:
-            self.chaksunT = data
+        self.InfoList[index] = data
     
     def GetInfo(self, index):
-        if index == 0:
-            return self.hrName
-        elif index == 1:
-            return self.hrNo
-        elif index == 2:
-            return self.name
-        elif index == 3:
-            return self.age
-        elif index == 4:
-            return self.sex
-        elif index == 5:
-            return self.ord
-        elif index == 6:
-            return self.owName
-        elif index == 7:
-            return self.owNo
-        elif index == 8:
-            return self.birthday
-        elif index == 9:
-            return self.rank
-        elif index == 10:
-            return self.faHrName
-        elif index == 11:
-            return self.faHrNo
-        elif index == 12:
-            return self.moHrName
-        elif index == 13:
-            return self.moHrNo
-        elif index == 14:
-            return self.rcCntT
-        elif index == 15:
-            return self.ord1CntT
-        elif index == 16:
-            return self.ord2CntT
-        elif index == 17:
-            return self.ord3CntT
-        elif index == 18:
-            return self.rcCntY
-        elif index == 19:
-            return self.ord1CntY
-        elif index == 20:
-            return self.ord2CntY
-        elif index == 21:
-            return self.ord3CntY
-        elif index == 22:
-            return self.chaksunT
+        return self.InfoList[index]
 
-    def printInfo(self):
-        print(self.hrName, self.hrNo, self.name, self.age, self.sex, self.ord, self.owName, self.owNo)
-        print(self.birthday, self.rank, self.faHrName, self.faHrNo, self.moHrName, self.moHrNo)
-        print(self.rcCntT, self.ord1CntT, self.ord2CntT, self.ord3CntT)
-        print(self.rcCntY, self.ord1CntY, self.ord2CntY, self.ord3CntY)
-        print(self.chaksunT)
+    def GetInfoList(self):
+        return self.InfoList
