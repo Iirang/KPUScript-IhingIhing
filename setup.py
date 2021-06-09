4221c81f556f6e945572f8a8f878552b5b66ae79
DESKTOP-KJDL4HB\USER <meekk1009@gmail.com>
2021-06-09 22:27:28 +0900

    배포판 만들기

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+
+module1 = Extension('spammodule',
+                    sources = ['spammodule.c'])
+
+setup(
+    name='Ihing-Ihing',
+    version='1.0',
+    py_modules=['main', 'Horse', 'Owner', 'Map', 'raceResult', 'raceHorseInfo', 'horseOwnerInfo', 'TelegramBot'],
+    #ext_modules = [module1]
+   ) 
