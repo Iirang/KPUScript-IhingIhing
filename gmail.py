013abbeb9f184713429c3e51edc92fece07e7901
Iirang <70070272+Iirang@users.noreply.github.com>
2021-06-09 14:49:39 +0900

    gmail 수정본 및 로고
    추가된 리소스 이름
    텔레그램: Telegram
    지메일: Gmail
    지도: Map

--- gmail.py
+++ gmail.py
@@ -7,8 +7,7 @@ import smtplib
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from raceHorseInfo import *
-from raceResult import *
+from main import *
 
 def GetMail():
     #global value
@@ -21,25 +20,25 @@ def GetMail():
 
     # 제목, 내용, 보내는 사람 / 받는 사람
     msg = MIMEMultipart("multipart", "alternative")
-    msg['Subject'] = "[한국마사회X한국산업기술대학교] 요청하신 정보를 보내드립니다."
+    msg['Subject'] = "[한국마사회X한국산업기술대학교] 요청하신" + " 정보를 보내드립니다."
     msg['From'] = senderAddr
     msg['To'] = recipientAddr
 
     # HTML문서 생성
     html = """\
-    <html>
-      <head></head>
-      <body>
-        <p>Hi!<br>
-           How are you?<br>
-           Here is the <a href="http://www.python.org">link</a> you wanted.
-        </p>
-      </body>
-    </html>
-    """
+        <html>
+          <head></head>
+          <body>
+            <p>Hi!<br>
+               How are you?<br>
+               Here is the <a href="http://www.python.org">link</a> you wanted.
+            </p>
+          </body>
+        </html>
+        """
 
     # html text로 변환
-    part2 = MIMEText(html, 'html')
+    part2 = MIMEText(html, 'html', _charset='UTF-8')
     # html msg에 붙히기
     msg.attach(part2)
 
